42a8869925e010c1e7a6a867c72036417e39fa34
Dorta Wallis <dorota.wallis@gmail.com>
2013-07-18 21:46:04 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var p = new Buffer(fs.readFileSync('index.html'), "utf-8");
+var buf = new Buffer(fs.readFileSync('index.html'), "utf-8");
 
 app.get('/', function(request, response) {
   response.send(buf.toString());
